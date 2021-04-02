0f11ee5308fbb48206908a7d7c09a084350419f7
None <None>
2021-04-02 08:34:54 -0700

    trying to get correct data... not working...

--- dumbbell_topology.py
+++ dumbbell_topology.py
@@ -16,7 +16,7 @@ class Dumbbell_Topology(Topo):
 
 	def build(self, delay=2):
 		""" 
-		For all calculations, we assume a packet size (MTU) of 1500 bytes.
+		For all calculations, we assume a 12000 bits/packet and 1500-byte packets
 		"""
 
 		# Backbone Router 1
@@ -34,7 +34,7 @@ class Dumbbell_Topology(Topo):
 		"""
 		# Bandwidth is in Mbps, delay is in ms, and max queue size is in packets
 		# Connect Backbone Router 1 to Backbone Router 2
-		self.addLink(s1, s2, cls=TCLink, bw=984, delay='{0}ms'.format(delay), max_queue_size=82*delay, use_htb=True)
+		self.addLink(s1, s2, bw=984, delay='{0}ms'.format(delay))
 		
 		"""
 		The access routers can transmit/receive at 252Mbps (21p/ms).
@@ -43,9 +43,9 @@ class Dumbbell_Topology(Topo):
 		"""
 		# Bandwidth is in Mbps, delay is in ms, and max queue size is in packets
 		# Connect Access Router 1 to Backbone Router 1
-		self.addLink(s1, s3, cls=TCLink, bw=252, delay='0ms', max_queue_size=21*delay*0.2, use_htb=True)
+		self.addLink(s1, s3, bw=252, delay='0ms', max_queue_size=21*delay*0.2)
 		# Connect Access Router 2 to Backbone Router 2
-		self.addLink(s2, s4, cls=TCLink, bw=252, delay='0ms', max_queue_size=21*delay*0.2, use_htb=True)
+		self.addLink(s2, s4, bw=252, delay='0ms', max_queue_size=21*delay*0.2)
 		
 		# Source 1
 		h1 = self.addHost('h1')
@@ -62,18 +62,18 @@ class Dumbbell_Topology(Topo):
 		"""
 		# Bandwidth is in Mbps, delay is in ms, and max queue size is in packets
 		# Connect Source 1 to Access Router 1
-		self.addLink(h1, s3, cls=TCLink, bw=960, delay='0ms', max_queue_size=80*delay, use_htb=True)
+		self.addLink(h1, s3, bw=960, delay='0ms')
 		# Connect Source 2 to Access Router 1
-		self.addLink(h2, s3, cls=TCLink, bw=960, delay='0ms', max_queue_size=80*delay, use_htb=True)
+		self.addLink(h2, s3, bw=960, delay='0ms')
 		# Connect Receiver 1 to Access Router 2
-		self.addLink(h3, s4, cls=TCLink, bw=960, delay='0ms', max_queue_size=80*delay, use_htb=True)
+		self.addLink(h3, s4, bw=960, delay='0ms')
 		# Connect Receiver 2 to Access Router 2
-		self.addLink(h4, s4, cls=TCLink, bw=960, delay='0ms', max_queue_size=80*delay, use_htb=True)
+		self.addLink(h4, s4, bw=960, delay='0ms')
 
 	
 def run_tests():
 	topo = Dumbbell_Topology(delay=21)
-	net = Mininet(topo=topo)
+	net = Mininet(topo=topo, link=TCLink)
 	net.start()
 	
 	print("Dumping host connections")
@@ -107,7 +107,7 @@ def run_tests():
 	
 def run_tcp_tests_cwnd(algorithm, delay):
 	topo = Dumbbell_Topology(delay)
-	net = Mininet(topo=topo)
+	net = Mininet(topo=topo, link=TCLink)
 	net.start()
 	
 	h1, h2, h3, h4 = net.getNodeByName('h1', 'h2', 'h3', 'h4')
@@ -128,18 +128,24 @@ def run_tcp_tests_cwnd(algorithm, delay):
 	time.sleep(5)
 	
 	print('Starting iperf client h1')
-	popens[h1] = h1.popen('iperf3 -c {0} -p 5566 -t 1904 -C {1} -i 1 > cwnd_{2}_{3}_{4}'.format(h3.IP(), algorithm, algorithm, h1, delay), shell=True)
-	print('238 delay for client h2')
-	for i in range(238,0,-1):
+	popens[h1] = h1.popen('iperf3 -c {0} -p 5566 -t 200 -C {1} -i 1 > cwnd_{2}_{3}_{4}'.format(h3.IP(), algorithm, algorithm, h1, delay), shell=True)
+	print('200 delay for client h2')
+	for i in range(40,0,-1):
 		time.sleep(1)
 		print(i)
 	print('Starting iperf client h2')
-	popens[h2] = h2.popen('iperf3 -c {0} -p 5566 -t 1666 -C {1} -i 1 > cwnd_{2}_{3}_{4}'.format(h4.IP(), algorithm, algorithm, h2, delay), shell=True)
+	popens[h2] = h2.popen('iperf3 -c {0} -p 5566 -t 160 -C {1} -i 1 > cwnd_{2}_{3}_{4}'.format(h4.IP(), algorithm, algorithm, h2, delay), shell=True)
 
 	print("Waiting for clients to finish...")
 	
-	popens[h1].communicate()
-	popens[h2].communicate()
+	try:
+		popens[h1].communicate(timeout=200)
+	except subprocess.TimeoutExpired as e:
+		popens[h1].kill()
+	try:
+		popens[h2].communicate(timeout=200)
+	except subprocess.TimeoutExpired as e:
+		popens[h2].kill()
 	popens[h3].terminate()
 	popens[h4].terminate()
 	
@@ -174,9 +180,9 @@ def run_tcp_tests_fairness(algorithm, delay):
 	time.sleep(5)
 	
 	print('Starting iperf client h1')
-	popens[h1] = h1.popen('iperf3 -c {0} -p 5566 -t 1000 -C {1}'.format(h3.IP(), algorithm), shell=True)
+	popens[h1] = h1.popen('iperf3 -c {0} -p 5566 -t 100 -C {1}'.format(h3.IP(), algorithm), shell=True)
 	print('Starting iperf client h2')
-	popens[h2] = h2.popen('iperf3 -c {0} -p 5566 -t 1000 -C {1}'.format(h4.IP(), algorithm), shell=True)
+	popens[h2] = h2.popen('iperf3 -c {0} -p 5566 -t 100 -C {1}'.format(h4.IP(), algorithm), shell=True)
 	
 	print("Waiting for clients to finish...")
 		
@@ -201,8 +207,8 @@ def gather_data(algorithm, delay, cwnd):
 		if (path.exists("{0}_h2_{1}_cwnd_new".format(algorithm, delay))):
 			os.remove("{0}_h2_{1}_cwnd_new".format(algorithm, delay))
 		print("Creating the files for IPERF to plot the CWND graph")
-		subprocess.Popen("cat cwnd_{0}_h1_{1} | grep sec | head -2000 | tr - \" \" | awk '{{ if ($12 == \"KBytes\")print $4, int($11)/(12*1024); else print $4, int($11)/12;}}'> {2}_h1_{3}_cwnd_new".format(algorithm,delay,algorithm,delay), shell=True)
-		subprocess.Popen("cat cwnd_{0}_h2_{1} | grep sec | head -1750 | tr - \" \" | awk '{{ if ($12 == \"KBytes\")print $4+250, int($11)/(12*1024); else print $4+250, int($11)/12;}}' > {2}_h2_{3}_cwnd_new".format(algorithm,delay,algorithm,delay), shell=True)
+		subprocess.Popen("cat cwnd_{0}_h1_{1} | grep sec | head -200 | tr - \" \" | awk '{{ if ($12 == \"KBytes\")print $4, int($11)/(8000*12000); else if ($12 == \"MBytes\")print $4, int($11)/(8000000*12000); else print $4, int($11)/(8*12000);}}'> {2}_h1_{3}_cwnd_new".format(algorithm,delay,algorithm,delay), shell=True)
+		subprocess.Popen("cat cwnd_{0}_h2_{1} | grep sec | head -200 | tr - \" \" | awk '{{ if ($12 == \"KBytes\")print $4+40, int($11)/(8000*12000); else if ($12 == \"MBytes\")print $4, int($11)/(8000000*12000); else print $4+40, int($11)/(8*12000);}}' > {2}_h2_{3}_cwnd_new".format(algorithm,delay,algorithm,delay), shell=True)
 		print("Done") 
 	else:
 		if (path.exists("{0}_h3_{1}_fair_new".format(algorithm, delay))):
@@ -210,8 +216,8 @@ def gather_data(algorithm, delay, cwnd):
 		if (path.exists("{0}_h4_{1}_fair_new".format(algorithm, delay))):
 			os.remove("{0}_h4_{1}_fair_new".format(algorithm, delay))
 		print("Creating the files for IPERF to plot the TCP fairness graph")
-		subprocess.Popen("cat fair_{0}_h3_{1} | grep sec | head -1000 | tr - \" \" | awk '{{print $4, $8}}' > {2}_h3_{3}_fair_new".format(algorithm,delay,algorithm,delay), shell=True)
-		subprocess.Popen("cat fair_{0}_h4_{1} | grep sec | head -1000 | tr - \" \" | awk '{{print $4, $8}}' > {2}_h4_{3}_fair_new".format(algorithm,delay,algorithm,delay), shell=True)
+		subprocess.Popen("cat fair_{0}_h3_{1} | grep sec | head -100 | tr - \" \" | awk '{{print $4, $8}}' > {2}_h3_{3}_fair_new".format(algorithm,delay,algorithm,delay), shell=True)
+		subprocess.Popen("cat fair_{0}_h4_{1} | grep sec | head -100 | tr - \" \" | awk '{{print $4, $8}}' > {2}_h4_{3}_fair_new".format(algorithm,delay,algorithm,delay), shell=True)
 		print("Done") 
 
 
@@ -222,8 +228,8 @@ def plot_iperf(algorithm, delay, cwnd):
 		print("Creating the plots for IPERF for the CWND graph")
 		plot1 = subprocess.Popen(["gnuplot"], stdin=subprocess.PIPE, encoding='utf8')
 		plot1.stdin.write("plot \"{0}_h1_{1}_cwnd_new\" title \"TCP Flow 1\" with linespoints, \"{0}_h2_{1}_cwnd_new\" title \"TCP Flow 2\" with linespoints\n".format(algorithm, delay))
-		plot1.stdin.write("set xrange[1:2000]\n")
-		plot1.stdin.write("set xtics 1,1,2000\n")
+		plot1.stdin.write("set xrange[1:200]\n")
+		plot1.stdin.write("set xtics 1,20,200\n")
 		plot1.stdin.write("set title \"Change in cwnd (packets) vs Time (1s units) for two TCP flows (rtt = {0} ms) using {1}\"\n".format(delay*2, algorithm.upper()))
 		plot1.stdin.write("set xlabel \"Time (seconds)\"\n")
 		plot1.stdin.write("set ylabel \"Congestion Window (packets)\"\n")
@@ -238,8 +244,8 @@ def plot_iperf(algorithm, delay, cwnd):
 		plot1 = subprocess.Popen(["gnuplot"], stdin=subprocess.PIPE, encoding='utf8')
 		plot1.stdin.write("plot \"{0}_h3_{1}_fair_new\" title \"TCP Flow 1\" with linespoints, \"{0}_h4_{1}_fair_new\" title \"TCP Flow 2\" with linespoints\n".format(algorithm, delay))
 
-		plot1.stdin.write("set xrange[1:1000]\n")
-		plot1.stdin.write("set xtics 1,1,1000\n")
+		plot1.stdin.write("set xrange[1:100]\n")
+		plot1.stdin.write("set xtics 1,10,100\n")
 		plot1.stdin.write("set title \"Change in Throughput (Mbps) vs Time (1s units) for two TCP flows (rtt = {0} ms) using {1}\"\n".format(delay*2, algorithm.upper()))
 		plot1.stdin.write("set xlabel \"Time (seconds)\"\n")
 		plot1.stdin.write("set ylabel \"Throughput (Mbps)\"\n")
@@ -251,16 +257,18 @@ def plot_iperf(algorithm, delay, cwnd):
 	
 if __name__ == '__main__':
 	delay = [21, 81, 162]
-	algorithm = ['reno', 'cubic', 'westwood', 'vegas']
+	algorithm = ['cubic', 'reno' 'westwood', 'vegas']
 	
 	setLogLevel('info')
 	
 	#run_tests()
 	
-	for x in algorithm:
-		for y in delay:
-			print("CWND for {0} {1}".format(x, y))
-			run_tcp_tests_cwnd(x, y)
-			print("TCP Fairness for {0} {1}".format(x, y))
-			run_tcp_tests_fairness(x, y)
+	#for x in algorithm:
+		#for y in delay:
+			#print("CWND for {0} {1}".format(x, y))
+			#run_tcp_tests_cwnd(x, y)
+			#print("TCP Fairness for {0} {1}".format(x, y))
+			#run_tcp_tests_fairness(x, y)
+	print("CWND for {0} {1}".format(algorithm[0], delay[0]))
+	run_tcp_tests_cwnd(algorithm[0], delay[0])
 
